bb4ee9c19ec47d174732470c90a865c3623769bf
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-17 19:13:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ function increment(a) {
 function decrement(a) {
   return (a -= 1)
 }
-function makeInt(n) {
-  return parseInt(n, 10)
+function makeInt(string) {
+  return parseInt(string, 10)
 }
-function preserveDecimal(n) {
-  return parseFloat(n, 10)
+function preserveDecimal(string) {
+  return parseFloat(string)
 }
